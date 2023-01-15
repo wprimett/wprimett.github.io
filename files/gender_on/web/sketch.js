26aa74765add0d952da8d46a96e30af4b6f98fa9
Prîm <w.primett@campus.fct.unl.pt>
2023-01-15 14:46:32 +0100

    Update sketch.js

--- files/gender_on/web/sketch.js
+++ files/gender_on/web/sketch.js
@@ -1,8 +1,9 @@
 const rate = 800;
 
 function setup() {
-	  createCanvas(500, 500);
+	  createCanvas(windowWidth, windowHeight);
     noStroke()
+		textSize(16);
 }
 
 function draw() {
@@ -23,13 +24,14 @@ function draw() {
     var totalDeaths = parseInt(totalMinutes * 0.4 + 20)
         // var totalDeaths = rate;
     fill(100, 255)
-    text('recorded deaths due to maternal complications ' + currentDay  + '-' + currentMonth + '-' + currentYear + ': \n' + parseInt(totalDeaths) ,5, height - 25);
+		var textDate = currentDay  + '-' + currentMonth + '-' + currentYear;
+    text('Recorded deaths due to maternal complications | ' + textDate + ' \n' + parseInt(totalDeaths)  + '\n', (interval * 2) + (interval*width/(interval*2)) + width/padding + width/padding, 25);
 
     fill(100, millisecond%1000)
     for(let i = 0; i < totalDeaths; i++){
       const row = parseInt(i/interval);
       const col = i%interval;
-      let x = (col * 3) + (col*width/(interval*1.1)) + width/padding
+      let x = (col * 2) + (col*width/(interval*2)) + width/padding
       let y = (row) * (height/interval) + height / padding
       fill(100, 255)
       rect(x, y, 3, 8);
@@ -38,8 +40,8 @@ function draw() {
     let finalDeath = totalDeaths
     const row = parseInt(finalDeath/interval);
     const col = finalDeath%interval;
-    let x = (col * 3) + (col*width/(interval*1.1)) + width/padding
+    let x = (col * 2) + (col*width/(interval*2)) + width/padding
     let y = (row) * (height/interval) + height / padding
-    fill(222,0,0, millisecond%2000)
+    fill(222,0,0, millisecond%1500)
     rect(x, y, 3, 9);
 }
